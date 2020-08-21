ffd65a3aea13827a7e35c340419f4fec07e6d5e0
omer190 <70034840+omer190@users.noreply.github.com>
2020-08-21 22:20:27 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-python bot.py +[phone1] doge
-python bot.py +[phone1] zec
-python bot.py +[phone1] ltc
-python bot.py +[phone1] btc
-python bot.py +[phone1] bch
+python bot.py +[905434532199] doge
+python bot.py +[905434532199] zec
+python bot.py +[905434532199] ltc
+python bot.py +[905434532199] btc
+python bot.py +[905434532199] bch
 clear
 pwd
 bash start.sh
 
 #Replace [phone] with your Number Phone
 #ext 
-   python bot.py +62xxxx doge
+   python bot.py +905434532199 doge
